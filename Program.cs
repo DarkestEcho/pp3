4c8ef16c0f3a4221b38ecc4ef043456a2e9e9728
DarkestEcho <60607354+DarkestEcho@users.noreply.github.com>
2020-05-15 19:59:46 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace лб3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(":)");
+            new Task3().Run();
+        }
+    }
+    public class Task3
+    {
+        public void Run()
+        {
+            var tokenSource = new CancellationTokenSource();
+            var token = tokenSource.Token;
+            new Task(()=>{
+
+                while(!token.IsCancellationRequested)
+                {
+                    Console.WriteLine(" *-* ");
+                    Thread.Sleep(400);
+                }
+            }).Start();
+        Thread.Sleep(1000);
+        tokenSource.Cancel();
+
+        }
+    }
+}
